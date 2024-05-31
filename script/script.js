5d3f72a27b86c7dc02d5fc6e55af278abf85d300
동호 <65016499+dongho9@users.noreply.github.com>
2024-05-31 15:41:43 +0900

    0531 업데이트

--- script/script.js
+++ script/script.js
@@ -13,3 +13,20 @@ $(document).ready(function(){
         window.location.href = 'login.html'
     })
 })
+
+let window_scrolling;
+
+window.addEventListener('scroll', () => {
+  if (!window_scrolling) {
+    console.log('start wheeling!');
+    document.documentElement.classList.remove('scroll-hidden');
+  }
+
+  // 일정시간 뒤에 스크롤 동작 멈춤을 감지
+  clearTimeout(window_scrolling);
+  window_scrolling = setTimeout(() => {
+    console.log('stop wheeling!');
+    window_scrolling = undefined;
+    document.documentElement.classList.add('scroll-hidden');
+  }, 400);
+});
